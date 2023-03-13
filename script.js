ad2e0185ab8783a85472fd5aa0a0d95dcfb12573
nickhoban <110017566+nickhoban@users.noreply.github.com>
2023-03-13 17:51:23 +0800

    PU&sh

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var square = document.getElementById('square');
+
+function changeColour(colour) {
+    square.background = colour;
+}
+
+square.addEventListener('click', (event) => changeColour('green'));
+
 function createBuzzwordPhrase() {
     /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
     let buzz = ["Paradigm-changing", "Multi-tier", "10,000-foot", "Agile", "Customer", "Win-win"];
